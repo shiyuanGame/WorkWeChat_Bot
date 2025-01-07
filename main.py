90d840fdbdb28f6f779411ca04e95c0107479b6b
shiyuanGame <shiyuanGames@126.com>
2025-01-07 11:00:20 +0800

    更新

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ API_KEY = 'api_key'  # 你需要替换为自己的 API 密钥
 
 URL = f"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={API_KEY}"
 
-response = requests.get(URL)
-data = response.json()
-print(data)
+
+def send_reminder():
+    webhook_url = URL
+    message = {
+        "msgtype": "text",
+        "text": {
+            "content": "@所有人 工时系统填写！",
+            # "mentioned_list": ["@all"]
+        }
+    }
+    requests.post(webhook_url, json=message)
+
+
+send_reminder()
